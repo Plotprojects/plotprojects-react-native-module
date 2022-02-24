ea40981ee809361fcc3af8d9ad71838d0cee50cc
Yahya Al fayad <yahya.alfayad@plotprojects.com>
2022-02-24 15:21:01 +0100

    Support removing handlers
    * Support removing notification filter
    * Support removing geotrigger handler
    * Support removing notification open handler
    * Fix issue causing the filter calls to be skipped
    * Update version to 1.0.8

--- index.js
+++ index.js
@@ -4,16 +4,49 @@ const { PlotProjectsReactModule } = NativeModules;
 
 const eventEmitter = new NativeEventEmitter(NativeModules.ToastExample);
 
+PlotProjectsReactModule.unregisterNotificationFilter = function() {
+	if (PlotProjectsReactModule.notificationFilterListener) {
+		PlotProjectsReactModule.notificationFilterListener.remove();
+		PlotProjectsReactModule.unsetNotificationFilterRegistered();
+		console.debug("Notification filter was removed successfully");
+	}
+}
+
 PlotProjectsReactModule.registerNotificationFilter = function(callback) {
-    eventEmitter.addListener('onNotificationsToFilter', (notificationsWithBatchId) => callback(notificationsWithBatchId.batchId, notificationsWithBatchId.data));
+	PlotProjectsReactModule.unregisterNotificationFilter();
+    PlotProjectsReactModule.notificationFilterListener = eventEmitter.addListener('onNotificationsToFilter', (notificationsWithBatchId) => callback(notificationsWithBatchId.batchId, notificationsWithBatchId.data));
+	PlotProjectsReactModule.setNotificationFilterRegistered();
+	console.debug("Notification filter was registered successfully");
 };
+
+PlotProjectsReactModule.unregisterGeotriggerHandler = function() {
+	if (PlotProjectsReactModule.geotriggerHandler) {
+		PlotProjectsReactModule.geotriggerHandler.remove();
+		PlotProjectsReactModule.unsetGeotriggerHandlerRegistered();
+		console.debug("Geotrigger handler was removed successfully");
+	}
+}
 
 PlotProjectsReactModule.registerGeotriggerHandler = function(callback) {
-    eventEmitter.addListener('onGeotriggersToHandle', (geotriggersWithBatchId) => callback(geotriggersWithBatchId.batchId, geotriggersWithBatchId.data));
+	PlotProjectsReactModule.unregisterGeotriggerHandler();
+    PlotProjectsReactModule.geotriggerHandler = eventEmitter.addListener('onGeotriggersToHandle', (geotriggersWithBatchId) => callback(geotriggersWithBatchId.batchId, geotriggersWithBatchId.data));
+	PlotProjectsReactModule.setGeotriggerHandlerRegistered();
+	console.debug("Geotrigger handler was registered successfully");
 };
 
+PlotProjectsReactModule.unregisterNotificationOpenHandler = function() {
+	if (PlotProjectsReactModule.notificationOpenHandler) {
+		PlotProjectsReactModule.notificationOpenHandler.remove();
+		PlotProjectsReactModule.unsetNotificationOpenHandlerRegistered();
+		console.debug("Notification open handler was removed successfully");
+	}
+}
+
 PlotProjectsReactModule.registerNotificationOpenHandler = function(callback) {
-    eventEmitter.addListener('onNotificationOpened', (notification) => callback(notification));
+	PlotProjectsReactModule.unregisterNotificationOpenHandler();
+    PlotProjectsReactModule.notificationOpenHandler = eventEmitter.addListener('onNotificationOpened', (notification) => callback(notification));
+	PlotProjectsReactModule.setNotificationOpenHandlerRegistered();
+	console.debug("Notification open handler was registered successfully");
 };
 
 export default PlotProjectsReactModule;
